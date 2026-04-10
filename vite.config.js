0584820af522893fd96f642f42db7bda873f5d5b
KOTHA MAHEEDHAR <seshadrinath54054@gmail.com>
2026-04-10 20:22:48 +0530

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  // GitHub Pages serves this project from /sorrything/
+  base: command === 'build' ? '/sorrything/' : '/',
+}))
